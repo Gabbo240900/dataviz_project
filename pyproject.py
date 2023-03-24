c12c7d13bfc092c90e750786a009f8b73e42307a
Fratra21 <127875599+Fratra21@users.noreply.github.com>
2023-03-24 13:46:49 +0100

    Python code
    Importing and cleaning of the dataset

---
+++ pyproject.py
@@ -0,0 +1,95 @@
+# Let's import the main libraries!
+import pandas as pd
+import numpy as np
+import datetime
+
+
+
+# Let's read the dataset and print an initial overview
+df_airplanes = pd.read_csv('International_Report_Departures.csv')
+df_airplanes.head()
+
+# Let's check the nature of our values
+# Print data types of each column
+print(df_airplanes.dtypes)
+# Check for missing values
+print(df_airplanes.isnull().sum())
+
+#There are 3055obs which lack of 'carrier' obj.. since we have over 900k observations,
+#we can just drop them.
+df_airplanes = df_airplanes.dropna()
+
+# Check for missing values
+print(df_airplanes.isnull().sum())
+#All good, 0  missing values!
+
+# Check for duplicates
+duplicates = df_airplanes.duplicated()
+print(f"Number of duplicate rows: {duplicates.sum()}")
+# No duplicates in our dataframe, great!
+
+# The name of the columns are not explanatory, thus we rename them
+df_airplanes = df_airplanes.rename(columns={'data_dte': 'Date',
+                                            'usg_apt_id': 'US_airport_ID',
+                                            'usg_apt': 'US_airport_code',
+                                            'usg_wac': 'US_World_Area_Code',
+                                            'fg_apt_id': 'Foreign_airport_ID',
+                                            'fg_apt': 'Foreign_airport_code',
+                                            'fg_wac': 'Foreign_World_Area_Code',
+                                            'airlineid': 'Airline_ID',
+                                            'carrier': 'Carrier',
+                                            'carriergroup': 'Carrier_of_US',
+                                            'Scheduled': 'Scheduled_Flights',
+                                            'Charter': 'Charter_Flights',
+                                            'Total': 'Total_Flights'})
+
+# It seems that 'type' column has only equal values, let's check it!
+if df_airplanes['type'].nunique() == 1:
+    print("'type' column contains only equal values.")
+
+
+#Let's drop 'type' column because it has all equal values
+df_airplanes = df_airplanes.drop('type', axis=1)
+
+df_airplanes['Date'] = (pd.to_datetime(df_airplanes['Date'], format='%m/%d/%Y', errors='coerce'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
